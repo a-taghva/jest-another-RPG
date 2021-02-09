2fa2b0ae1d38cdf91d66f76f0307940e4c042975
a-taghva <taghvali99@gmail.com>
2021-02-09 14:16:20 -0500

    isAlive() method

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -19,4 +19,21 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
+});
+
+test('gets player\'s health value', () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('check if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
 })
+
